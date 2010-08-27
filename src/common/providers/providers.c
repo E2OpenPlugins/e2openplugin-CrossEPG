e5f54dafc4c17c853bea1effb04e098f07fbe93e
sandro@skanetwork.com <sandro@skanetwork.com@bda989b6-6649-11de-9c70-efa4e9705d34>
2010-08-27 11:37:08 +0000

    (common) committed missing files
    git-svn-id: http://crossepg.googlecode.com/svn/trunk@64 bda989b6-6649-11de-9c70-efa4e9705d34

---
+++ src/common/providers/providers.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <strings.h>
+#include <memory.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+#include "../../common.h"
+
+#include "../core/log.h"
+
+#include "providers.h"
+
+static int protocol = 0; // 0 unknown, 1 opentv, 2 xmltv
+static int channels_pids[64];
+static int channels_pids_count = 0;
+static int titles_pids[64];
+static int titles_pids_count = 0;
+static int summaries_pids[64];
+static int summaries_pids_count = 0;
+static int channels_types[64];
+static int channels_types_count = 0;
+static int nid = 0;
+static int tsid = 0;
+static int sid = 0;
+static char channels[256];
+static char url[256];
+static char preferred_language[4];
+
+int  *providers_get_channels_pids		()	{ return channels_pids;			}
+int  *providers_get_titles_pids			()	{ return titles_pids;			}
+int  *providers_get_summaries_pids		()	{ return summaries_pids;		}
+int  *providers_get_channels_types		()	{ return channels_types;		}
+int  providers_get_channels_pids_count	()	{ return channels_pids_count;	}
+int  providers_get_titles_pids_count	()	{ return titles_pids_count;		}
+int  providers_get_summaries_pids_count	()	{ return summaries_pids_count;	}
+int  providers_get_channels_types_count	()	{ return channels_types_count;	}
+int  providers_get_nid					()	{ return nid;					}
+int  providers_get_tsid					()	{ return tsid;					}
+int  providers_get_sid					()	{ return sid;					}
+int  providers_get_protocol				()	{ return protocol;				}
+char *providers_get_xmltv_channels		()	{ return channels;				}
+char *providers_get_xmltv_url			()	{ return url;					}
+char *providers_get_xmltv_plang			()	{ return preferred_language;	}
+
+static char *providers_trim_spaces (char *text)
+{
+	char *tmp = text;
+	while (tmp[0] == ' ') tmp++;
+	while (strlen (tmp) > 1)
+		if (tmp[strlen (tmp) - 1] == ' ') tmp[strlen (tmp) - 1] = '\0';
+		else break;
+	
+	if (tmp[0] == ' ') tmp[0] = '\0';
+	return tmp;
+}
+
+bool providers_read (char *read)
+{
+	FILE *fd = NULL;
+	char line[512];
+	char key[256];
+	char value[256];
+	
+	channels_pids_count = 0;
+	titles_pids_count = 0;
+	summaries_pids_count = 0;
+	channels_types_count = 0;
+	protocol = 0;
+	
+	strcpy (channels, "");
+	strcpy (url, "");
+	strcpy (preferred_language, "eng");
+	
+	fd = fopen (read, "r");
+	if (!fd) 
+		return false;
+	
+	while (fgets (line, sizeof(line), fd)) 
+	{
+		char *tmp_key, *tmp_value;
+		
+		memset (key, 0, sizeof (key));
+		memset (value, 0, sizeof (value));
+		
+		if (sscanf (line, "%[^#=]=%s\n", key, value) != 2)
+			continue;
+		
+		tmp_key = providers_trim_spaces (key);
+		tmp_value = providers_trim_spaces (value);
+		
+		if (strcmp ("protocol", tmp_key) == 0)
+		{
+			if (strcmp ("opentv", tmp_value) == 0)
+				protocol = 1;
+			else if (strcmp ("xmltv", tmp_value) == 0)
+				protocol = 2;
+		}
+		else if (strcmp ("channels_pids", tmp_key) == 0)
+		{
+			char* tmp = strtok (tmp_value, "|");
+			while ((tmp != NULL) && (channels_pids_count < 64))
+			{
+				channels_pids[channels_pids_count] = atoi (tmp);
+				tmp = strtok (NULL, "|");
+				channels_pids_count++;
+			}
+		}
+		else if (strcmp ("titles_pids", tmp_key) == 0)
+		{
+			char* tmp = strtok (tmp_value, "|");
+			while ((tmp != NULL) && (titles_pids_count < 64))
+			{
+				titles_pids[titles_pids_count] = atoi (tmp);
+				tmp = strtok (NULL, "|");
+				titles_pids_count++;
+			}
+		}
+		else if (strcmp ("summaries_pids", tmp_key) == 0)
+		{
+			char* tmp = strtok (tmp_value, "|");
+			while ((tmp != NULL) && (summaries_pids_count < 64))
+			{
+				summaries_pids[summaries_pids_count] = atoi (tmp);
+				tmp = strtok (NULL, "|");
+				summaries_pids_count++;
+			}
+		}
+		else if (strcmp ("channels_types", tmp_key) == 0)
+		{
+			char* tmp = strtok (tmp_value, "|");
+			while ((tmp != NULL) && (channels_types_count < 64))
+			{
+				channels_types[channels_types_count] = atoi (tmp);
+				tmp = strtok (NULL, "|");
+				channels_types_count++;
+			}
+		}
+		else if (strcmp ("nid", tmp_key) == 0)
+			nid = atoi (tmp_value);
+		else if (strcmp ("tsid", tmp_key) == 0)
+			tsid = atoi (tmp_value);
+		else if (strcmp ("sid", tmp_key) == 0)
+			sid = atoi (tmp_value);
+		else if (strcmp ("channels", tmp_key) == 0)
+			strcpy (channels, tmp_value);
+		else if (strcmp ("url", tmp_key) == 0)
+			strcpy (url, tmp_value);
+		else if (strcmp ("preferred_language", tmp_key) == 0)
+			if (strlen (tmp_value) == 3)
+				strcpy (preferred_language, tmp_value);
+	}
+	
+	fclose (fd);
+	
+	return true;
+}
---
+++ src/common/providers/providers.h
@@ -0,0 +1,21 @@
+#ifndef _OPENTV_PROVIDERS_H_
+#define _OPENTV_PROVIDERS_H_
+
+int  *providers_get_channels_pids ();
+int  *providers_get_titles_pids ();
+int  *providers_get_summaries_pids ();
+int  *providers_get_channels_types ();
+int  providers_get_channels_pids_count ();
+int  providers_get_titles_pids_count ();
+int  providers_get_summaries_pids_count ();
+int  providers_get_channels_types_count ();
+int  providers_get_nid ();
+int  providers_get_tsid ();
+int  providers_get_sid ();
+int  providers_get_protocol ();
+bool providers_read (char *read);
+char *providers_get_xmltv_channels ();
+char *providers_get_xmltv_url ();
+char *providers_get_xmltv_plang ();
+
+#endif // _OPENTV_PROVIDERS_H_
