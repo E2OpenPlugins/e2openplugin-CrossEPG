964d0d6d3411c1c271f946f8642cb4e4f09a95c7
sandro@skanetwork.com <sandro@skanetwork.com@bda989b6-6649-11de-9c70-efa4e9705d34>
2010-08-27 11:41:53 +0000

    (common) commited missed file compileb.py
    git-svn-id: http://crossepg.googlecode.com/svn/trunk@65 bda989b6-6649-11de-9c70-efa4e9705d34

---
+++ compileb.py
@@ -0,0 +1,12 @@
+import py_compile
+import dircache
+try:
+	list = dircache.listdir("src/enigma2/python/")
+	for file in list:
+		if len(file) > 3 and file[-3:] == ".py":
+			print file
+			py_compile.compile("src/enigma2/python/%s" % (file),
+								"src/enigma2/python/%so" % (file),
+								"/usr/lib/enigma2/python/Plugins/SystemPlugins/CrossEPG/%s" % (file))
+except Exception, e:
+	print e
