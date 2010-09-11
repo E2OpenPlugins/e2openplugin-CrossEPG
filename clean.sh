6c87b2508263d40db4d04d7094d020ccc3b855ba
sandro@skanetwork.com <sandro@skanetwork.com@bda989b6-6649-11de-9c70-efa4e9705d34>
2010-09-11 18:47:47 +0000

    Added make scripts for sh4 Deleted old useless files and folders
    git-svn-id: http://crossepg.googlecode.com/svn/trunk@79 bda989b6-6649-11de-9c70-efa4e9705d34

--- clean.sh
+++ clean.sh
@@ -5,33 +5,6 @@ find . | grep "\._" | xargs rm 2>/dev/null
 find . | grep "\.DS_Store" | xargs rm 2>/dev/null
 find . | grep "\.o$" | xargs rm 2>/dev/null
 
-make -f Makefiles/crossepg.sh4 clean
-make -f Makefiles/crossepgd.sh4 clean
-make -f Makefiles/crossepg_dbconverter.enigma2.sh4 clean
-make -f Makefiles/crossepg_dbconverter.enigma2.mipsel clean
-make -f Makefiles/crossepg_dbconverter.enigma2.ppc clean
-make -f Makefiles/crossepg_downloader.sh4 clean
-make -f Makefiles/crossepg_downloader.enigma2.sh4 clean
-make -f Makefiles/crossepg_downloader.enigma2.mipsel clean
-make -f Makefiles/crossepg_downloader.enigma2.ppc clean
-make -f Makefiles/crossepg_dbinfo.enigma2.sh4 clean
-make -f Makefiles/crossepg_dbinfo.enigma2.mipsel clean
-make -f Makefiles/crossepg_dbinfo.enigma2.ppc clean
-make -f Makefiles/crossepg_epgcopy.enigma2.sh4 clean
-make -f Makefiles/crossepg_epgcopy.enigma2.mipsel clean
-make -f Makefiles/crossepg_epgcopy.enigma2.ppc clean
-make -f Makefiles/crossepg_importer.sh4 clean
-make -f Makefiles/crossepg_importer.enigma2.sh4 clean
-make -f Makefiles/crossepg_importer.enigma2.mipsel clean
-make -f Makefiles/crossepg_importer.enigma2.ppc clean
-make -f Makefiles/crossepg_sync.sh4 clean
-make -f Makefiles/crossepg_launcher.sh4 clean
-
-rm -rf enigma2/sh4/usr/lib/enigma2/python/Plugins/SystemPlugins/CrossEPG/*
-rm -rf enigma2/mipsel/usr/lib/enigma2/python/Plugins/SystemPlugins/CrossEPG/*
-rm -rf enigma2/sh4/var/crossepg/providers
-rm -rf enigma2/mipsel/usr/crossepg/providers
-rm src/enigma2/python/*.pyc
-rm src/enigma2/python/*.pyo
+make clean
 
 exit 0
---
+++ make_e2_sh4.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+CROSS=/opt/STM/STLinux-2.3/devkit/sh4/bin/sh4-linux-
+CFLAGS+=-I/opt/STM/STLinux-2.3/devkit/sh4/target/usr/include/libxml2/ \
+CC=${CROSS}gcc \
+STRIP=${CROSS}strip \
+make
+
+[ -d tmp ] && rm -rf tmp
+
+mkdir -p tmp/CONTROL
+mkdir -p tmp/var/crossepg/aliases
+mkdir -p tmp/var/crossepg/providers
+mkdir -p tmp/var/crossepg/import
+mkdir -p tmp/usr/lib/enigma2/python/Plugins/SystemPlugins/CrossEPG/skins
+
+cp bin/* tmp/var/crossepg/
+
+cp providers/* tmp/var/crossepg/providers/
+cp src/enigma2/python/skins/*.xml tmp/usr/lib/enigma2/python/Plugins/SystemPlugins/CrossEPG/skins/
+cp contrib/crossepg_epgmove.sh tmp/var/crossepg/
+cp contrib/control tmp/CONTROL/
+
+chmod 755 tmp/var/crossepg/crossepg_*
+
+# copy with tar so we clean .svn entries
+cd contrib
+tar --exclude-vcs -zc po | tar -zx -C ../tmp/usr/lib/enigma2/python/Plugins/SystemPlugins/CrossEPG/
+cd ..
+
+python compileb.py
+
+VERSION=`cat src/version.h | grep RELEASE | sed "s/.*RELEASE \"//" | sed "s/\"//" | sed "s/\ /-/" | sed "s/\ /-/" | sed "s/(//" | sed "s/)//"`
+echo "Version: $VERSION" >> tmp/CONTROL/control
+echo "Architecture: sh4" >> tmp/CONTROL/control
+
+sh ipkg-build -o root -g root tmp/
+
+[ ! -d out ] && mkdir out
+mv *.ipk out
