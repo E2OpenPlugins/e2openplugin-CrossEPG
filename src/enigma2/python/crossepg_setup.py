5531246976cc69ec8d4a089a33c84a2b2c73d020
Andy Blackburn <andyblac@o2.co.uk>
2012-02-15 00:39:37 +0000

    add warning when using flash as epg data location.
    Signed-off-by: Andy Blackburn <andyblac@o2.co.uk>

--- src/enigma2/python/crossepg_setup.py
+++ src/enigma2/python/crossepg_setup.py
@@ -127,7 +127,7 @@ class CrossEPG_Setup(Screen):
 		return False
 		
 	def showWarning(self):	
-		self.session.open(MessageBox, _("PLEASE READ!\nNo disk found. An hard drive or an usb pen is HARDLY SUGGESTED. If you still want use your internal flash pay attention to:\n(1) If you don't have enough free space your box may completely block and you need to flash it again\n(2) Many write operations on your internal flash may damage your flash memory"), type = MessageBox.TYPE_ERROR)
+		self.session.open(MessageBox, _("PLEASE READ!\nA hard drive or an usb pen is HARDLY SUGGESTED. If you still want use your internal flash pay attention to:\n(1) If you don't have enough free space your box may completely block and you need to flash it again\n(2) Many write operations on your internal flash may damage your flash memory"), type = MessageBox.TYPE_ERROR)
 	
 	def keyLeft(self):
 		self["config"].handleKey(KEY_LEFT)
@@ -161,6 +161,7 @@ class CrossEPG_Setup(Screen):
 	def makeList(self):
 		self.list = []
 
+		self.config.db_root = config.misc.epgcachepath.value + 'crossepg'
 		#device_default = None
 		#i = 0
 		#for mountpoint in self.mountpoint:
@@ -333,7 +334,7 @@ class CrossEPG_Setup(Screen):
 			
 		crossepg_auto.forcePoll()
 		
-		if self.config.db_root == self.config.home_directory + "/data" and not self.config.isQBOXHD():
+		if (self.config.db_root == self.config.home_directory + "/data" and not self.config.isQBOXHD()) or self.config.db_root.startswith('/etc/enigma2'):
 			self.showWarning()
 			
 		self.close()
