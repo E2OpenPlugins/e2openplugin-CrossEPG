82c070fc635ebc2e06f1166281497e28185b19ba
Andy Blackburn <andrew.blackburn644@btinternet.com>
2013-11-12 21:54:56 +0000

    allow to compile without poll, makes download slower as PID’s have to be read in order, but some drivers have issues with the original way.
    todo, make CrossEPG download a background task so the E2 gui is not locked, so it does not matter how long download takes.

--- src/common/dvb/dvb.c
+++ src/common/dvb/dvb.c
@@ -26,6 +26,107 @@
 
 void dvb_read (dvb_t *settings, bool(*data_callback)(int, unsigned char*))
 {
+#ifdef NO_DVB_POLL
+	int i;
+	for (i = 0; i < settings->pids_count; i++)
+	{
+		int cycles, total_size, fd;
+		struct dmx_sct_filter_params params;
+		dmx_source_t ssource;
+		
+		char first[settings->buffer_size];
+		int first_length;
+		bool first_ok;
+		
+		switch (settings->frontend)
+		{
+		case 1:
+			ssource = DMX_SOURCE_FRONT1;
+			break;
+		case 2:
+			ssource = DMX_SOURCE_FRONT2;
+			break;
+		case 3:
+			ssource = DMX_SOURCE_FRONT3;
+			break;
+		default:
+			ssource = DMX_SOURCE_FRONT0;
+		}
+		
+		memset(&params, 0, sizeof(params));
+		params.pid = settings->pids[i];
+		params.filter.filter[0] = settings->filter;
+		params.filter.mask[0] = settings->mask;
+		params.timeout = 5000;
+		params.flags = DMX_IMMEDIATE_START | DMX_CHECK_CRC;
+
+		if ((fd = open(settings->demuxer, O_RDWR | O_NONBLOCK)) < 0) {
+			log_add ("Cannot open demuxer '%s'", settings->demuxer);
+			continue;
+		}
+
+		if (ioctl(fd, DMX_SET_SOURCE, &ssource) == -1) {
+			log_add ("ioctl DMX_SET_SOURCE failed");
+			close(fd);
+			continue;
+		}
+
+		if (ioctl(fd, DMX_SET_FILTER, &params) == -1) {
+			log_add ("ioctl DMX_SET_FILTER failed");
+			close(fd);
+			continue;
+		}
+
+		log_add ("Reading pid 0x%x...", params.pid);
+
+		first_length = 0;
+		first_ok = false;
+		total_size = 0;
+		cycles = 0;
+		while (cycles < MAX_OTV_LOOP_CYCLES)
+		{
+			int k;
+			bool force_quit = false;
+			unsigned char buf[settings->buffer_size];	// 4K buffer size
+			int size = read (fd, buf, sizeof(buf));
+
+			if (size == -1) {
+				usleep (10 * 1000);
+				continue;
+			}
+
+			if (size < settings->min_length) continue;
+				
+			if (first_length == 0)
+			{
+				first_length = size;
+				memcpy (first, buf, size);
+			}
+			else if (first_length == size)
+			{
+				if (memcmp (buf, first, size) == 0) {
+					first_ok = true;
+				}
+			}
+
+			total_size += size;
+			//data_callback (size, buf);
+			if (!data_callback (size, buf)) {
+				log_add ("Forced to quit");
+				break;
+			}
+
+			if (first_ok) {
+				log_add ("Done");
+				break;
+			}
+			
+			cycles++;
+		}
+
+		close(fd);
+	}
+#else
 	struct pollfd PFD[256];
 	int cycles, i, total_size;
 	struct dmx_sct_filter_params params;
@@ -123,4 +224,5 @@ void dvb_read (dvb_t *settings, bool(*data_callback)(int, unsigned char*))
 			log_add ("Error stopping filter");
 		close (PFD[i].fd);
 	}
+#endif
 }
